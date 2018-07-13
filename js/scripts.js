2b66d9f319351617949311a16d71ecfe4883ff08
Obure Joseph <oburejoseph@outlook.com>
2018-07-13 09:03:48 +0300

    js form login form

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,64 @@
 
-// BUSINESS LOGIC
-//
-//
-// PROTOTYPE
-//
-//
-// UI LOGIC
 $(document).ready(function(){
-$()
+	$("#btn").click(function(){
+		event.preventDefault();
+
+
+	})
+
 })
-// =======
-function SignUp(email){
-  this.email = email
-}
 
 
 
-$(document).ready(function() {
 
-    $('#submit').click(function() {
-      event.preventDefault();
 
-        $(".error").show();
-        var hasError = false;
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-        var emailaddress = $("#email").val();
-        // var SignUp = new newEmail(emailaddress);
 
-        if(emailaddress == '') {
-            $("#email").after('<span class="error">Please enter your email address.</span>');
-            hasError = true;
-        }
-
-        else if(!emailReg.test(emailaddress)) {
-            $("#email").after('<span class="error">Enter a valid email address.</span>');
-            hasError = true;
-        }
-        $("#email").val();
-        if(hasError == true) { return false; }
-
-    });
-});
-// >>>>>>> jose
+// $(".login").addClass(fadeOut);
+// $(".login").show();
+// $(".login").addClass(fadeIn)
+//
+// // BUSINESS LOGIC
+// //
+// //
+// // PROTOTYPE
+// //
+// //
+// // UI LOGIC
+// $(document).ready(function(){
+// $()
+// })
+// // =======
+// function SignUp(email){
+//   this.email = email
+// }
+//
+//
+//
+// $(document).ready(function() {
+//
+//     $('#submit').click(function() {
+//       event.preventDefault();
+//
+//         $(".error").show();
+//         var hasError = false;
+//         var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//
+//         var emailaddress = $("#email").val();
+//         // var SignUp = new newEmail(emailaddress);
+//
+//         if(emailaddress == '') {
+//             $("#email").after('<span class="error">Please enter your email address.</span>');
+//             hasError = true;
+//         }
+//
+//         else if(!emailReg.test(emailaddress)) {
+//             $("#email").after('<span class="error">Enter a valid email address.</span>');
+//             hasError = true;
+//         }
+//         $("#email").val();
+//         if(hasError == true) { return false; }
+//
+//     });
+// });
+//
